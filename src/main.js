f560b0cefd2a456cb90aa99ba6c156701dbd2f46
Nodirbek <nodirbekibragimov2005.@gmail.com>
2024-02-23 15:34:58 +0500

    vue blog end

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import UiComponents from './Ui-components'
+import store from './store'
 const app = createApp(App)
 UiComponents.map(component => app.component(component.name, component))
 app.use(router)
+app.use(store)
 
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,30 @@
 <template>
     <div>
-        home
+        <h1>Home {{ counter }}</h1>
+        <div class="w-25">
+            <Button @click="increment">Toggle</Button>
+            <Button @click="dincrement">Toggle</Button>
+        </div>
     </div>
 </template>
 <script>
+import Button from '@/Ui-components/Button.vue';
+
 export default {
-    
+    components: { Button },
+    methods:{
+        increment(){
+            this.$store.commit('increment')
+        },
+        dincrement(){
+            this.$store.commit('dincrement')
+        }
+    },
+    computed:{
+       counter(){
+        return this.$store.state.count
+       } 
+    }
 }
 </script>
-<style scoped>
-    
-</style>
+<style scoped></style>
